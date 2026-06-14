3b7295a337d9150b90a34298376be72b8b2a3717
joykat05 <riakataria195@gmail.com>
2026-06-14 22:50:16 +0530

    chore: initialize project structure with React + Vite frontend, Tailwind CSS config, and empty backend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './app.css'
+function App() {
+
+  return (
+    <>
+     <div className="bg-lime-100 m-auto rounded-2xl w-1/2 h-1/2 flex justify-center items-center">
+      <h1>Herbal Trace</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
